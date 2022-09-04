6dc338179b5b3b1ba53db9d6bed66792aa5fa48c
kimriri <dmsal547@gmail.com>
2022-09-04 18:43:56 +0900

    DateBinding & Xml

--- app/src/main/java/com/programmers/myapplication/MainActivity.kt
+++ app/src/main/java/com/programmers/myapplication/MainActivity.kt
@@ -24,15 +24,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
         myNumberViewModel = ViewModelProvider(this)[MyNumberViewModel::class.java]
         binding.viewModel = myNumberViewModel
+
         myNumberViewModel.currenValue.observe(this) {
             Log.d(TAG, "MainActivityValue : $it")
             binding.numberTextView.text = it.toString()
         }
-
-//        binding.plusBtn.setOnClickListener{
-//            myNumberViewModel.updateValue(ActionType.PULS,editTextDefault(binding = binding))}
-//        binding.minusBtn.setOnClickListener{
-//            myNumberViewModel.updateValue(ActionType.MINUS,editTextDefault(binding = binding))}
 
         binding.goFragmentBtn.setOnClickListener {
             supportFragmentManager.commit {
@@ -40,13 +36,5 @@ class MainActivity : AppCompatActivity() {
                 replace<SubFragment>(R.id.fragment_container_view)
             }
         }
-    }
-
-    private fun editTextDefault(binding: ActivityMainBinding): String {
-        when {
-            binding.userInputEdittext.text.isEmpty() ->
-                binding.userInputEdittext.setText(DefaultInternalObject.StrZero)
-        }
-            return binding.userInputEdittext.text.toString()
     }
 }
